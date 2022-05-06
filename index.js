9316fe8d63c86635a561d4613d5ed1b2f0fbb527
Alex Agrelo <alex.agrelo@gmail.com>
2022-05-06 15:27:19 +0100

    formulario y tabla en local

---
+++ index.js
@@ -0,0 +1,66 @@
+let data = [];
+
+function addNewInput(subject, teacher, weeklyHours) {
+    // console.log('addNewInput');
+    let newInput = {
+        subject: subject,
+        teacher: teacher,
+        weeklyHours: weeklyHours
+    }
+    data.push(newInput);
+    console.log('data: ', data);
+}
+
+function paintTable() {
+    // console.log('paintTable');
+    table = document.getElementById("dataTable");
+    tbody = document.querySelector("#dataTable tbody");
+    tbody.innerHTML = "";
+    for (let i = 0; i < data.length; i++) {
+        let row = table.insertRow(i);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerHTML = data[i].subject;
+        cell2.innerHTML = data[i].teacher;
+        cell3.innerHTML = data[i].weeklyHours;
+
+        tbody.appendChild(row);
+    }
+}
+
+function saveData(e) {
+    // console.log('saveData');
+    e.preventDefault();
+    let subject = document.getElementById("inputSubject").value;
+    let teacher = document.getElementById("inputTeacher").value;
+    let weeklyHours = document.getElementById("inputWeeklyHours").value;
+    addNewInput(subject, teacher, weeklyHours);
+    paintTable();
+}
+
+function reqListener() {
+    console.log(this.responseText);
+}
+
+/* function updateJson() {
+    var req = new XMLHttpRequest();
+    req.open("GET", "horarios.json");
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                console.log('status 200 readyState 4');
+                console.log(req.responseText);
+
+            }
+            else {
+                console.log("Error loading file\n");
+            }
+        }
+    }
+    req.send(null);
+} */
+
+document.getElementById("btnSubmit").addEventListener("click", saveData);
+paintTable();
+
